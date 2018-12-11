80080be7b91d65c0aee6133d2c611f52572cf69b
Kolos Gergely <kolos460@gmail.com>
2018-12-11 14:47:20 +0100

    time funciton updated

--- memorygamefinal.py
+++ memorygamefinal.py
@@ -1,7 +1,9 @@
 
 import time
+import os
+import sys
 
-cards = (" ","@","ß","Đ","€","§","$","Ł","&")
+cards = (" ", "@", "ß", "Đ", "€", "§", "$", "Ł", "&")
 
 board_coor = {'A1': cards[0], 'A2': cards[0], 'A3': cards[0], 'A4': cards[0],
               'B1': cards[0], 'B2': cards[0], 'B3': cards[0], 'B4': cards[0],
@@ -9,25 +11,24 @@ board_coor = {'A1': cards[0], 'A2': cards[0], 'A3': cards[0], 'A4': cards[0],
               'D1': cards[0], 'D2': cards[0], 'D3': cards[0], 'D4': cards[0]}
 
 
-
 def print_board():
     print("")
     print('     1   2   3   4  ')
     print('   -----------------')
     print('   |   |   |   |   |')
-    print(' A | ' + board_coor['A1'] + ' | ' + board_coor['A2'] + ' | ' + board_coor['A3']+ ' | ' + board_coor['A4']+ ' | ')
+    print(' A | ' + board_coor['A1'] + ' | ' + board_coor['A2'] + ' | ' + board_coor['A3'] + ' | ' + board_coor['A4'] + ' | ')
     print('   |   |   |   |   |')
     print('   -----------------')
     print('   |   |   |   |   |')
-    print(' B | ' + board_coor['B1'] + ' | ' + board_coor['B2']+ ' | ' + board_coor['B3']+ ' | ' + board_coor['B4']+ ' | ')
+    print(' B | ' + board_coor['B1'] + ' | ' + board_coor['B2'] + ' | ' + board_coor['B3'] + ' | ' + board_coor['B4'] + ' | ')
     print('   |   |   |   |   |')
     print('   -----------------')
     print('   |   |   |   |   |')
-    print(' C | ' + board_coor['C1'] + ' | ' + board_coor['C2'] + ' | ' + board_coor['C3']+ ' | ' + board_coor['C4']+ ' | ')
+    print(' C | ' + board_coor['C1'] + ' | ' + board_coor['C2'] + ' | ' + board_coor['C3'] + ' | ' + board_coor['C4'] + ' | ')
     print('   |   |   |   |   |')
     print('   -----------------')
     print('   |   |   |   |   |')
-    print(' D | ' + board_coor['D1'] + ' | ' + board_coor['D2']+ ' | ' + board_coor['D3']+ ' | ' + board_coor['D4']+ ' | ')
+    print(' D | ' + board_coor['D1'] + ' | ' + board_coor['D2'] + ' | ' + board_coor['D3'] + ' | ' + board_coor['D4'] + ' | ')
     print('   |   |   |   |   |')
     print('   -----------------')
     print("")
@@ -37,7 +38,6 @@ def card_turn():
     mark1 = input("Type the card's coordinate to turn it: ").upper()
     while mark1 not in board_coor:
         mark1 = input("Type the card's coordinate to turn it: ").upper()
-
 
     if mark1 == 'A1':
         board_coor['A1'] = cards[1]
@@ -71,7 +71,6 @@ def card_turn():
         board_coor['D3'] = cards[1]
     elif mark1 == 'D4':
         board_coor['D4'] = cards[1]
-    
 
     mark2 = input("Type the second card's coordinate to turn it up: ").upper()
     while mark2 not in board_coor:
@@ -109,61 +108,73 @@ def card_turn():
         board_coor['D3'] = cards[1]
     elif mark2 == 'D4':
         board_coor['D4'] = cards[1]
-    
+
     marks = [mark1, mark2]
     return marks
+
 
 def compare(marks):
     if marks[0] == marks[1]:
         print("You cannot turn same cards - try again!")
         board_coor[marks[0]] = emptycard
-    elif marks[0]!= marks[1]:
+    elif marks[0] != marks[1]:
         if board_coor[marks[0]] == board_coor[marks[1]]:
             print("Congrats - it's a match!")
         else:
             print("No match - try again!")
             board_coor[marks[0]] = emptycard
             board_coor[marks[1]] = emptycard
- 
+            os.system('clear')
+
 
 def gameend():
     if str(" ") not in board_coor.values():
-        print("You win! Cheers!")
-        continuegame = input("Would you like to continue? If yes, type 'y' or 'no' to exit the game: ")
+        continuegame = input("Would you like to continue? If yes, type 'y' or 'n' to exit the game: ")
         if continuegame == 'y' or continuegame == 'Y':
-            board_coor['A1'] = cards[1]
-            board_coor['A2'] = cards[1]
-            board_coor['A3'] = cards[1]
-            board_coor['A4'] = cards[1]
-            board_coor['B1'] = cards[1]
-            board_coor['B2'] = cards[1]
-            board_coor['B3'] = cards[1]
-            board_coor['B4'] = cards[1]
-            board_coor['C1'] = cards[1]
-            board_coor['C2'] = cards[1]
-            board_coor['C3'] = cards[1]
-            board_coor['C4'] = cards[1]
-            board_coor['D1'] = cards[1]
-            board_coor['D2'] = cards[1]
-            board_coor['D3'] = cards[1]
-            board_coor['D4'] = cards[1]
+            board_coor['A1'] = cards[0]
+            board_coor['A2'] = cards[0]
+            board_coor['A3'] = cards[0]
+            board_coor['A4'] = cards[0]
+            board_coor['B1'] = cards[0]
+            board_coor['B2'] = cards[0]
+            board_coor['B3'] = cards[0]
+            board_coor['B4'] = cards[0]
+            board_coor['C1'] = cards[0]
+            board_coor['C2'] = cards[0]
+            board_coor['C3'] = cards[0]
+            board_coor['C4'] = cards[0]
+            board_coor['D1'] = cards[0]
+            board_coor['D2'] = cards[0]
+            board_coor['D3'] = cards[0]
+            board_coor['D4'] = cards[0]
+            os.system('clear')
+            timer()
         if continuegame == 'n' or continuegame == 'N':
+            os.system('clear')
             print("See you!")
+            sys.exit()
 
-def score():
-    elapsed_time = time.time() - start_time
-    time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
-    if elapsed_time <= 100:
-        print("You have great memory")
-    elif elapsed_time >= 101:
-        print("Improve your memory! Play again!")
 
+def timer():
+    end = time.time()
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("You did it! Your time is: ")
+    print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+    #if elapsed_time <= 100:
+    #    print("You did it! Yout time is: ")
+    #    print(elapsed_time)
+    #    print("You have a great memory!")
+    #elif elapsed_time >= 101:
+    #    print("Improve your memory! Play again!")
+
+
+#def output_clearing
    
 while True:
+    start = time.time()
     print_board()
-    start_time = time.time()
     marks = card_turn()
     print_board()
     compare(marks)
     gameend()
-    score()
